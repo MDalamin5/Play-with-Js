f3c390fe3f4a3bee30f6aa7462f27f92f2fa33e8
MDalamin5 <mdal.amin5@northsouth.edu>
2023-11-16 02:18:56 +0600

    problem solving

--- Module 01/Week-1/Day-3/Problem-01/main.js
+++ Module 01/Week-1/Day-3/Problem-01/main.js
@@ -27,3 +27,30 @@ stu.sort(function(a, b){
     return a - b;
 });
 stu.forEach(printData);
+
+// check a year leap year or not
+
+let year = 2024;
+function isLeapYear(year){
+    if(year % 400 === 0 || (year % 4 === 0 && year % 100 === 0)){
+        console.log(year, 'is Leap year');
+    }
+    else
+    console.log('Not');
+}
+isLeapYear(year);
+
+// Check in a sentence how many vowel is there
+
+const vowel = ["a", "e", "i", "o", "u"];
+function cechkVowel(sentence){
+    let count = 0;
+    const letter = Array.from(sentence);
+    letter.forEach(function(value){
+        if(vowel.includes(value)){
+            count++;
+        }
+    });
+    console.log(count);
+}
+cechkVowel("Hello sir how are you!!")
