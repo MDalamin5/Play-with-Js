7e4ee24c7ac19c395fe8f92b0fe4c8e440bc6cb4
MDalamin5 <mdal.amin5@northsouth.edu>
2023-11-16 17:37:59 +0600

    problem solving

--- Module 01/Week-1/Day-3/Problem-01/main.js
+++ Module 01/Week-1/Day-3/Problem-01/main.js
@@ -32,7 +32,7 @@ stu.forEach(printData);
 
 let year = 2024;
 function isLeapYear(year){
-    if(year % 400 === 0 || (year % 4 === 0 && year % 100 === 0)){
+    if(year % 400 === 0 || (year % 4 === 0 && year % 100 != 0)){
         console.log(year, 'is Leap year');
     }
     else
