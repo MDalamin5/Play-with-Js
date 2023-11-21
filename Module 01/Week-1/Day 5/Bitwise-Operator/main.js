906390172feb76b5908232d5a12223a301bb16a8
MDalamin5 <mdal.amin5@northsouth.edu>
2023-11-22 02:13:35 +0600

    Bitwise operators, Reguler types

---
+++ Module 01/Week-1/Day 5/Bitwise-Operator/main.js
@@ -0,0 +1,3 @@
+console.log(3 & 5);  
+//This is theoritical Tutorial
+//https://www.w3schools.com/js/js_bitwise.asp
---
+++ Module 01/Week-1/Day 5/Reguler Expressions/main.js
@@ -0,0 +1,6 @@
+// Searching text and Replace any word
+//. /pattern/modifers;
+let text = "visit w3School";
+let n = text.search(/W3School/i);  // 6
+console.log(n);
+// 8: 34 min --> #46 video
