6133f684916a6c172a2db62394cd91ca887a5299
MDalamin5 <mdal.amin5@northsouth.edu>
2023-11-29 12:06:09 +0600

    skope of js

--- Module 01/Week-1/Day 5/Exception-Handling/main.js
+++ Module 01/Week-1/Day 5/Exception-Handling/main.js
@@ -1,9 +1,29 @@
+// try{
+//     let x = 10; 
+//     let y = 0;
+//     console.log(x/y);
+// }
+// catch(err){
+//     console.log(err);
+// }
+// console.log('After the catch block');
+
+let y = 2;
+
 try{
-    let x = 10; 
-    let y = 0;
-    console.log(x/y);
+    if(y == ''){
+        throw 'Its empty';
+    }
+    if( y == NaN(y)) throw 'Not a Number';
+
+    if(y <= 5) throw 'Too low number';
+    if(y>10) throw {
+        message : 'Too high',
+        name : 'HighError'
+    };
 }
-catch(err){
+catch(err)
+{
     console.log(err);
 }
-console.log('After the catch block');
+console.log('After catch block');
---
+++ Module 01/Week-1/Day 5/Skope/main.js
@@ -0,0 +1,9 @@
+ console.log('Intro Of Skope');
+
+ let x = 2;
+ console.log(x*4);
+
+ {
+    let x = 4
+    console.log(4*x);
+ }
