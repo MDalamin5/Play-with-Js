ecf6eea47825fa535dd0d32490aa57b61b00bf84
MDalamin5 <mdal.amin5@northsouth.edu>
2023-12-02 18:53:10 +0600

    hosting in js

--- Module 01/Week-1/Day 5/Skope/main.js
+++ Module 01/Week-1/Day 5/Skope/main.js
@@ -7,3 +7,17 @@
     let x = 4
     console.log(4*x);
  }
+
+ // recape Scope
+ {
+   // block space this value is not use out side the block
+   var xy = 5;
+   console.log(xy - x);
+   var carName = 'volvo'
+ }
+ console.log(window.carName);
+ let carName1 = 'alion'
+ 
+
+
+ 
