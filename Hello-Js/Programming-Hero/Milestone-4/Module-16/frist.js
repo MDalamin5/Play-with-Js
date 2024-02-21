219cf2649d4564d487a0701d4127d564cb8bb0c4
MDalamin5 <mdal.amin5@northsouth.edu>
2024-02-21 20:26:32 +0600

    hello js

---
+++ Hello-Js/Programming-Hero/Milestone-4/Module-16/frist.js
@@ -0,0 +1,35 @@
+// console.log('Assalamualikum');
+
+// variable
+var num = 10;
+console.log(num);
+var namee = "Md Al Amin";
+console.log(namee);
+
+var result = true;
+console.log(result);
+
+person = {
+    'name' : 'Al Amin',
+    'age' : 23,
+    'uni' : 'NSU'
+}
+
+console.log(person.name);
+
+var tomoto_price = 30;
+tomoto_price = 50;
+
+var first_name = 'Md';
+var last_name = 'AL Amin'
+
+var full_name = full_name + last_name;
+console.log(full_name);
+
+
+var  price1 = 30;
+var price2 = '44';
+console.log(price1 + price2);
+
+price2 = parseInt(price2);
+console.log(price1 + price2);
---
+++ Hello-Js/Programming-Hero/Milestone-4/Module-16/typeOf.js
@@ -0,0 +1,20 @@
+// var num = 40;
+// console.log(typeof(num))
+// var namee= 'Md AL Amin'
+// console.log(typeof(namee))
+
+// console.log(typeof(true))
+// var hello;
+// console.log(typeof(hell));
+
+var numm = .2;
+var numm2 = .1;
+
+var sum = numm + numm2
+sum = sum.toFixed(1)
+sum = parseFloat(sum);
+console.log(sum)
+
+// reminder
+
+console.log(5%2);
