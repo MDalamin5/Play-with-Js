b68cc6fdbdc3522fa91871518dc7bf6362e61590
MDalamin5 <mdal.amin5@northsouth.edu>
2024-02-22 08:12:15 +0600

    repo update

--- Hello-Js/Programming-Hero/Milestone-4/Module-16/typeOf.js
+++ Hello-Js/Programming-Hero/Milestone-4/Module-16/typeOf.js
@@ -1,11 +1,11 @@
-// var num = 40;
-// console.log(typeof(num))
-// var namee= 'Md AL Amin'
-// console.log(typeof(namee))
+var num = 40;
+console.log(typeof(num))
+var namee= 'Md AL Amin'
+console.log(typeof(namee))
 
-// console.log(typeof(true))
-// var hello;
-// console.log(typeof(hell));
+console.log(typeof(true))
+var hello;
+console.log(typeof(hell));
 
 var numm = .2;
 var numm2 = .1;
