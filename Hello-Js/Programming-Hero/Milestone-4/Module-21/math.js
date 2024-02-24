e0d83559d7e4119887bf83ba83c2f61d8492d9fb
MDalamin5 <mdal.amin5@northsouth.edu>
2024-02-24 17:14:06 +0600

    js math functions

---
+++ Hello-Js/Programming-Hero/Milestone-4/Module-21/math.js
@@ -0,0 +1,19 @@
+const num1 = 40;
+const num2 = 46;
+
+console.log(Math.abs(num1 -num2));
+
+const newNumber = 55.44;
+console.log(Math.round(newNumber))
+
+console.log(Math.ceil(newNumber));
+console.log(Math.floor(newNumber));
+
+
+const randomNumber = Math.random();
+console.log(randomNumber)
+
+// Random number start from 3 and integer number
+
+const newNUm = Math.round(Math.random() * 100) + 3;
+console.log(newNUm)
