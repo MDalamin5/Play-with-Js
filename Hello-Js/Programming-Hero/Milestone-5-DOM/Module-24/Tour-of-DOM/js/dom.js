286177fcef2379bdff23be4c1ece233e8ddf9142
MDalamin5 <mdal.amin5@northsouth.edu>
2024-02-25 16:50:57 +0600

    play with js dom

--- Hello-Js/Programming-Hero/Milestone-5-DOM/Module-24/Tour-of-DOM/js/dom.js
+++ Hello-Js/Programming-Hero/Milestone-5-DOM/Module-24/Tour-of-DOM/js/dom.js
@@ -1,7 +1,7 @@
-// console.log('test');
-// console.log(document);
-// console.log(document.body);
-// console.log(document.getElementsByTagName('h1'));
+console.log('test');
+console.log(document);
+console.log(document.body);
+console.log(document.getElementsByTagName('h1'));
 const liElements = document.getElementsByTagName('li');
 for(const list of liElements){
     // console.log(list) 
@@ -13,3 +13,41 @@ for(const h1 of h1Elements){
     // console.log(list) 
     console.log(h1.innerText)
 }
+
+// get Elements by id
+
+const bodyItems = document.getElementById('body-item')
+// console.log(bodyItems.innerText)
+
+const myFevFood = document.getElementsByClassName('fev-food')
+// console.log(myFevFood)
+for(let food of myFevFood){
+    console.log(food.innerHTML)
+}
+
+let foodTitle = document.getElementById('food-title')
+foodTitle.innerText = 'Food tile update from js'
+testQuerySelectorData = document.querySelectorAll('.container li')
+// console.log(testQuerySelectorData);
+for(li of testQuerySelectorData){
+    console.log(li.innerText)
+}
+
+// bring only one elements form the class or id
+
+console.log('Data take from ')
+foodContainerData = document.querySelector('.food-container li')
+console.log(foodContainerData)
+
+document.getElementById('field-tit').style.backgroundColor = 'red'
+document.getElementById('field-tit').style.textAlign='center'
+
+const title = document.getElementById('field-tit')
+console.log(title.getAttribute('class'))
+console.log(title.classList)
+console.log(title.innerText)
+console.log(title.innerHTML)
+const food_title = document.getElementById('food-title');
+food_title.setAttribute('food-title', 'tooltip set by js')
+
+console.log(document.getElementsByClassName('food-container')[0].innerHTML)
