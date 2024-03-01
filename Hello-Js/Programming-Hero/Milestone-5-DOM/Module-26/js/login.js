94b5064e22ead8492788f14e4ffc4b3d81ec7c02
MDalamin5 <mdal.amin5@northsouth.edu>
2024-03-01 11:36:29 +0600

    login form connect on js

---
+++ Hello-Js/Programming-Hero/Milestone-5-DOM/Module-26/js/login.js
@@ -0,0 +1,17 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+    // to det the email address inside the email input field
+    // use .vale to get the text from an input field
+    const emailField = document.getElementById('user-email')
+    const email = emailField.value
+    console.log(email)
+
+    const passwordField = document.getElementById('user-password')
+    const password = passwordField.value
+    console.log(password)
+
+    if((email === 'alamin@gmail.com') && (password === 'hello1234')){
+        console.log('valid password')
+    }
+    else
+        console.log('invalid pass')
+})
