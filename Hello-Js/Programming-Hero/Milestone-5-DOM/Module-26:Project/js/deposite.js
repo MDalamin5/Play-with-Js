be3a2110b30b162f84ae9e75d1d88bfeaac434fd
MDalamin5 <mdal.amin5@northsouth.edu>
2024-03-04 13:37:16 +0600

    done all functionality and all working

--- Hello-Js/Programming-Hero/Milestone-5-DOM/Module-26:Project/js/deposite.js
+++ Hello-Js/Programming-Hero/Milestone-5-DOM/Module-26:Project/js/deposite.js
@@ -6,5 +6,45 @@ document.getElementById('btn-deposit').addEventListener('click', function(event)
     // get the previous amount
 
     const depo_total_element = document.getElementById('deposit-total')
-    depo_total_element.innerText = depo_amount
+
+    const previous_amount = parseInt(depo_total_element.innerText)
+    const total_amount = previous_amount + parseInt(depo_amount)
+    depo_total_element.innerText = total_amount
+    // console.log(typeof(depo_amount))
+
+    // update main balance
+    const curBalanceAmount = document.getElementById('total-balance')
+    const curBalanceAmountInt = parseInt(curBalanceAmount.innerText)
+    curBalanceAmount.innerText = curBalanceAmountInt + parseInt(depo_amount)
+
+    depo_input.value = ''
+})
+
+document.getElementById('btn-withdrow').addEventListener('click', function(){
+    const withdrowField = document.getElementById('user-withdrow')
+    const withdrowAmount = parseInt(withdrowField.value)
+    
+    const previousWithdrowData = document.getElementById('withdrow-total')
+    const previousWithdrowAmount = parseInt(previousWithdrowData.innerText)
+    console.log(previousWithdrowAmount)
+    const totalWithdrow = previousWithdrowAmount + withdrowAmount
+    console.log(totalWithdrow)
+    previousWithdrowData.innerText = totalWithdrow
+
+    // update main balance
+
+    const mainBalance = document.getElementById('total-balance')
+    let mainBalInt = parseInt(mainBalance.innerText)
+    console.log(typeof(withdrowAmount))
+    if(withdrowAmount <= mainBalInt){
+        mainBalInt = mainBalInt - withdrowAmount
+        mainBalance.innerText = mainBalInt
+    }
+    else
+        alert('Inscifient Balence please deposite first')
+
+
+
+
+    withdrowField.value = ''
 })
